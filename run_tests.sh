18f7e958cb7eccc37e011f9bf77ff1ed34d53c16
ShaheenMCW <mohamed.shaheen@multicorewareinc.com>
2024-12-18 13:25:23 +0530

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-# Compile the C program
 gcc -o matrix_driver driver.c -lm
 
-# Check if compilation was successful
 if [ $? -eq 0 ]; then
     echo "Compilation successful. Running tests..."
 
-    # Run the C program and capture the output
     ./matrix_driver
 
-    # Check if the execution was successful
     if [ $? -eq 0 ]; then
         echo "Tests completed successfully."
     else
